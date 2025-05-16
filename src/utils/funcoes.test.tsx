1214dd359f23c5dcafe7f3fc1ec5730e3fc49fc7
Willian Garcias <williangarcias10@gmail.com>
2025-05-16 16:28:42 -0300

    tests add

---
+++ src/utils/funcoes.test.tsx
@@ -0,0 +1,9 @@
+import { multiplica } from "./funcoes";
+
+test("Multiplicacao com dois numeros", () => {
+  expect(multiplica(2, 2)).toBe(4);
+});
+
+test("Multiplicacao com dois numeros", () => {
+  expect(multiplica(3, 3)).toBe(9);
+});
---
+++ src/utils/funcoes.tsx
@@ -0,0 +1,3 @@
+export function multiplica(a: number, b: number): number {
+  return a * b;
+}
